e4aa8839d40af862e795acde150c613605d5f650
TomFaust <tom.faust@hotmail.com>
2023-10-22 22:23:22 +0200

    Smaller icons on mobile

--- js/sprites.js
+++ js/sprites.js
@@ -15,36 +15,41 @@ let facts = [
   
 const myObject = {
     idle1: {
-        idle_left: { 
-            sprite: 'Me_idle_left.gif', 
-            delayRange: [2000, 10000] 
-        }
+      weight:1,
+      idle_left: { 
+          sprite: 'Me_idle_left.gif', 
+          delayRange: [2000, 10000] 
+      }
     },
     idle2: {
-        idle_right: { 
-            sprite: 'Me_idle_right.gif', 
-            delayRange: [2000, 10000] 
-        }
+      weight:1,
+      idle_right: { 
+          sprite: 'Me_idle_right.gif', 
+          delayRange: [2000, 10000] 
+      }
     },
     talk: {
-        alert: { 
-            sprite: 'Me_alert.gif', 
-            delayRange: [2000, 2000] 
-        },
-        talk: { 
-            sprite: 'Me_talk.gif', 
-            delayRange: [10000, 15000],
-            startFunction: startSpeech,
-            endFunction: endSpeech 
-        },
+      weight:1,
+      alert: { 
+          sprite: 'Me_alert.gif', 
+          delayRange: [2000, 2000] 
+      },
+      talk: { 
+          sprite: 'Me_talk.gif', 
+          delayRange: [10000, 15000],
+          startFunction: startSpeech,
+          endFunction: endSpeech 
+      },
     },
     yoyo: {
+      weight:1,
       yoyo:{
         sprite: 'Me_yoyo.gif',
         delayRange: [13500,13500],
       }
     },
     // paper_plane:{
+    //   weight:1,
     //   plan:{
     //     sprite: 'Me_paper_airplane.gif',
     //     delayRange: [14500,14500],
@@ -57,66 +62,87 @@ const myObject = {
     return Math.round(delay / 500) * 500; // Round delay to the nearest multiple of 0.5 seconds (500 milliseconds)
   }
 
-  async function iterateObjectWithDelay(obj) {
-    while (true) {
-      const groupKeys = Object.keys(obj);
-      const selectedGroupKey = groupKeys[Math.floor(Math.random() * groupKeys.length)];
-      const selectedGroup = obj[selectedGroupKey];
-      
-      const objectKeys = Object.keys(selectedGroup);
-      
-      for (const objectKey of objectKeys) {
-        const entry = selectedGroup[objectKey];
+ 
+async function iterateObjectWithDelay(obj) {
+  while (true) {
+    const groupKeys = Object.keys(obj);
+    const groupWeights = groupKeys.map(groupKey => ({ key: groupKey, weight: obj[groupKey].weight || 1 }));
+    const selectedGroupKey = weightedRandomSelection(groupWeights).key;
+    const selectedGroup = obj[selectedGroupKey];
 
-        if (document.hidden) {
-          await new Promise(resolve => {
-            document.addEventListener('visibilitychange', function handler() {
-              if (!document.hidden) {
-                document.removeEventListener('visibilitychange', handler);
-                resolve();
-              }
-            });
+    // Filter out the weight property from the object keys
+    const objectKeys = Object.keys(selectedGroup).filter(key => key !== 'weight');
+
+
+    for (const objectKey of objectKeys) {
+      const entry = selectedGroup[objectKey];
+
+      if (document.hidden) {
+        await new Promise(resolve => {
+          document.addEventListener('visibilitychange', function handler() {
+            if (!document.hidden) {
+              document.removeEventListener('visibilitychange', handler);
+              resolve();
+            }
           });
-        }
+        });
+      }
 
-        me.setAttribute("src","assets/Sprites/" + entry.sprite)
-        
-        if (entry.startFunction && typeof entry.startFunction === 'function') {
-          entry.startFunction(); // Execute start function
-        }
-        
-        const [minDelay, maxDelay] = entry.delayRange;
-        const delay = getRandomDelay(minDelay, maxDelay);
-        
-        // Wait for the specified delay before proceeding to the next iteration
-        await new Promise(resolve => setTimeout(resolve, delay));
-        
-        if (entry.endFunction && typeof entry.endFunction === 'function') {
-          entry.endFunction(); // Execute end function
-        }
+      me.setAttribute("src", "assets/Sprites/" + entry.sprite);
+
+      if (entry.startFunction && typeof entry.startFunction === 'function') {
+        entry.startFunction(); // Execute start function
+      }
+
+      const [minDelay, maxDelay] = entry.delayRange;
+      const delay = getRandomDelay(minDelay, maxDelay);
+
+      // Wait for the specified delay before proceeding to the next iteration
+      await new Promise(resolve => setTimeout(resolve, delay));
+
+      if (entry.endFunction && typeof entry.endFunction === 'function') {
+        entry.endFunction(); // Execute end function
       }
     }
   }
+}
 
-  async function startSpeech(){
-    let middle = document.getElementById('middle');
-    speech.style.visibility = 'visible';
-    middle.innerHTML = ""; 
-    let fact = facts[Math.floor(Math.random() * facts.length)];    
-    let factArray = fact.split("")
-    for (const letter of factArray) {
+async function startSpeech(){
+  let middle = document.getElementById('middle');
+  speech.style.visibility = 'visible';
+  middle.innerHTML = ""; 
+  let fact = facts[Math.floor(Math.random() * facts.length)];    
+  let factArray = fact.split("")
+  for (const letter of factArray) {
 
-      middle.innerHTML += letter;
+    middle.innerHTML += letter;
 
-      await new Promise(resolve => setTimeout(resolve, 40));
+    await new Promise(resolve => setTimeout(resolve, 40));
+  }
+}
+
+function endSpeech(){
+  speech.style.visibility = 'hidden';
+}
+
+
+function weightedRandomSelection(array) {
+  // Calculate the total weight of all items in the array
+  const totalWeight = array.reduce((acc, item) => acc + (item.weight || 1), 0);
+
+  // Generate a random number between 0 and the total weight
+  const randomValue = Math.random() * totalWeight;
+
+  // Loop through the array to find the item whose weight matches the randomValue
+  let cumulativeWeight = 0;
+  for (const item of array) {
+    cumulativeWeight += item.weight || 1;
+    if (randomValue <= cumulativeWeight) {
+      return item;
     }
   }
+}
 
-  function endSpeech(){
-    speech.style.visibility = 'hidden';
-  }
   
-  // Usage
-  iterateObjectWithDelay(myObject);
-
-
+// Usage
+iterateObjectWithDelay(myObject);
