fd818313d81b1ae1e785fc6f1f08ef76cc5612a7
TomFaust <tom.faust@hotmail.com>
2024-07-20 16:22:24 +0200

    cleanup and work on about me

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,11 @@
+//Javascript
 import "./timer.js"
 import "./customization.js";
 import "./sprites.js";
 import "./windows.js";
+import './language_switch.js'
 
+//CSS
 import '../css/style.css';
+import '../css/me.css';
 import '../css/windowSizes.css';
